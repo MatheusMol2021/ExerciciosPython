eb391e3bb8c4977864a77e11c2d4801d7fcdca7e
MatheusMol2021 <matheus@latinamerica.ind.br>
2022-03-09 16:39:13 -0300

    Concluído

--- 6.6.py
+++ 6.6.py
@@ -1,43 +1,22 @@
 
-último = 10
-último2 = 10
-fila = list(range(1, último + 1))
-fila2 = list(range(1, último2 + 1))
 
 while True:
-    print(f"\nExistem {len(fila)} clientes na fila 1")
-    print(f"Fila 1 atual: {fila}")
-    print(f"Existem {len(fila2)} clientes na fila 2")
-    print(f"Fila 2 atual: {fila2}")
-    print("Digite F,G para adicionar um cliente ao fim da fila ")
-    print("ou A, B para realizar o atendimento. S para sair. ")
-    operação = input("Operação (F, G, A, B ou S): ")
+    
+    a = input("Difite os parenteses() ou S para sair: ")
 
-    x = 0
-    while x < len(operação):
-        if operação[x] == "A":
-            if len(fila) > 0:
-                atendido = fila.pop(0)
-                print(f"Cliente {atendido} atendido")
-            else:
-                print("Fila vazia! Ninguém para atender.")
-        if operação[x] == "B":
-            if len(fila) > 0:
-                atendido2 = fila2.pop(0)
-                print(f"Cliente {atendido2} atendido")
-            else:
-                print("Fila vazia! Ninguém para atender.")
+    if a == "S":
+        break
 
+    x = 0 # conta os índices da pilha
+    y = 0 # Conta os parametros à esquerda ou à direita
+    while x < len(a):
+        if a[x] == "(":
+            y += 1
+        elif a[x] == ")":
+            y -= 1
+        x += 1
 
-        elif operação[x] == "F":
-            último += 1 # Incrementa o ticket do novo cliente
-            fila.append(último)
-        elif operação[x] == "G":
-            último2 += 1 # Incrementa o ticket do novo cliente
-            fila2.append(último2)
-
-        elif operação[x] == "S":
-            break
-        else:
-            print("Operação inválida! Digite F, A ou S!")
-        x +=1
+    if y != 0:
+        print("Erro")
+    else:
+        print("OK")
